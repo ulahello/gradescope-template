852066a28fa86a8ea9d2764af678c164549d400a
Ula Shipman <ula.hello@mailbox.org>
2024-07-24 13:16:18 -0700

    fix(build.sh): use new naming convention
    Signed-off-by: Ula Shipman <ula.hello@mailbox.org>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 rm -vf autograder.zip
 zip autograder.zip setup.sh run_autograder \
-    io_trace.py core.py cases.py recursion.py util.py autograder_script.py
+    io_trace.py core.py cases.py recursion.py util.py script_*.py
