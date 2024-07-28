5f9640f79b8347d007226415b2d8e43ce611c89a
Ula Shipman <ula.hello@mailbox.org>
2024-07-27 22:44:29 -0700

    feat(build.sh): name zip according to script
    Signed-off-by: Ula Shipman <ula.hello@mailbox.org>

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env sh
-rm -vf autograder.zip
-zip autograder.zip setup.sh run_autograder \
-    io_trace.py core.py cases.py recursion.py util.py script_*.py
+rm -vf autograder.zip zip_*.zip
+
+SCRIPT="`find . -name 'script_*.py' -type f | head -n 1`"
+if [ "${SCRIPT}" = "" ]; then
+	echo "${0}: Can't find an entry point! Does 'script_*.py' exist?"
+	exit 1
+fi
+
+# ./script_unit_section_exercise.py -> ./zip_unit_section_exercise.zip
+ZIP="`basename -s '.py' "${SCRIPT}"`"
+ZIP="${ZIP#script_}"
+ZIP="./zip_${ZIP}.zip"
+
+zip "${ZIP}" setup.sh run_autograder \
+    io_trace.py core.py cases.py recursion.py util.py "${SCRIPT}"
