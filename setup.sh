c7296a6b09afb967e683c4a1114e1091663594b1
Ula Shipman <ula.hello@mailbox.org>
2024-08-10 02:05:20 -0700

    bump to python 3.12
    Signed-off-by: Ula Shipman <ula.hello@mailbox.org>

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -e
 # This is my development environment. I try not to use version
 # specific stuff, but you can search for instances I'm aware of by
 # grepping for "@fragile".
-apt-get install python3.10 -y
+apt-get install python3.12 -y
