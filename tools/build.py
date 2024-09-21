c61d5b8cbd4f9694c55a179cd8977ed2bfd60368
Ula Shipman <ula.hello@mailbox.org>
2024-09-21 11:34:38 -0700

    feat(tools/build.py): use compression
    Signed-off-by: Ula Shipman <ula.hello@mailbox.org>

--- tools/build.py
+++ tools/build.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import shutil
 import sys
+import zipfile
 
 from typing import List, Optional, NoReturn, Tuple
 
@@ -16,7 +17,7 @@ def info(msg: str) -> None:
 
 def read_sources(parent_dir: str) -> List[str]:
     path = PurePath(parent_dir, "SOURCES")
-    sources: List[str] = []
+    sources = []
     with open(path, "r") as f:
         for line in f.read().splitlines(keepends=False):
             sources.append(line)
@@ -31,6 +32,34 @@ def find_script(script_dir: str) -> Optional[Tuple[str, str]]:
 def get_zip_name(script_name: str) -> str:
     new_name: str = "zip_" + script_name.removeprefix("script_")
     return PurePath(script_name).with_name(new_name).with_suffix(".zip").name
+
+def add_to_zip(zf: ZipFile, script_dir: str, sources: List[str]) -> None:
+    # add sources from script_dir
+    script_dir_sources = [
+        "setup.sh",
+        "run_autograder",
+    ]
+    for entry in os.scandir(script_dir):
+        if entry.name.endswith(".py") and entry.is_file():
+            script_dir_sources.append(entry.name)
+
+    for name in script_dir_sources:
+        path = PurePath(script_dir, name)
+        with open(path, "r") as f:
+            file_data = f.read()
+        zf.writestr(name, file_data)
+        info(f"added source '{name}'")
+
+    # add SOURCES. we change the cwd because sources are relative to the script directory.
+    old_cwd = os.getcwd()
+    os.chdir(script_dir)
+    for source_path in sources:
+        source_name: str = PurePath(source_path).name
+        with open(source_path, "r") as f:
+            source_data = f.read()
+        zf.writestr(source_name, source_data)
+        info(f"added source '{source_path}'")
+    os.chdir(old_cwd)
 
 assert get_zip_name("script_foo.whatever") == "zip_foo.zip"
 assert get_zip_name("script_unit_section_exercise.py") == "zip_unit_section_exercise.zip"
@@ -58,35 +87,24 @@ def build(script_dir: str, dst: str) -> None:
         pass
 
     # construct zip file
-    with ZipFile(zip_path, mode="w") as zipfile:
-        # add sources from script_dir
-        script_dir_sources = [
-            "setup.sh",
-            "run_autograder",
-        ]
-        for entry in os.scandir(script_dir):
-            if entry.name.endswith(".py") and entry.is_file():
-                script_dir_sources.append(entry.name)
+    for algo, level in [
+            (zipfile.ZIP_DEFLATED, 9),
+            (zipfile.ZIP_BZIP2, 9),
+            (zipfile.ZIP_LZMA, -1),
+            (zipfile.ZIP_STORED, -1),
+    ]:
+        try:
+            with ZipFile(zip_path, mode="w",
+                         compression=algo, compresslevel=level) as zf:
+                add_to_zip(zf, script_dir, sources)
 
-        for name in script_dir_sources:
-            path = PurePath(script_dir, name)
-            with open(path, "r") as f:
-                file_data = f.read()
-            zipfile.writestr(name, file_data)
-            info(f"added source '{name}'")
+            info(f"successfully built ZIP '{str(zip_path)}' ({algo=}, {level=})")
+            return
 
-        # add SOURCES. we change the cwd because sources are relative to the script directory.
-        old_cwd = os.getcwd()
-        os.chdir(script_dir)
-        for source_path in sources:
-            source_name: str = PurePath(source_path).name
-            with open(source_path, "r") as f:
-                source_data = f.read()
-            zipfile.writestr(source_name, source_data)
-            info(f"added source '{source_path}'")
-        os.chdir(old_cwd)
+        except RuntimeError:
+            continue
 
-    info(f"successfully built ZIP '{str(zip_path)}'")
+    fatal("no suitable compression method found. this is a bug.")
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Assemble a Gradescope compliant zip file. If not all source files are present, you must run 'populate.py' first.")
