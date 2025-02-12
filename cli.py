70a6bfc49267dd66136e56aae86e241387aa63f9
Ula <ula.hello@mailbox.org>
2025-02-12 19:21:26 +0000

    feat: CLI interface that can print summary of test cases (#6)
    * feat: optionally print test summary to stdout
    
    * chore: remove leftover comment
    
    * feat: less confusing naming convention for reporting results
    
    * chore: mark Summary{Good, Bad}.format as private
    
    * feat: set exit code based on pass/fail (only when --summary)

---
+++ cli.py
@@ -0,0 +1,28 @@
+from core import JsonMetadata, Case, autograder_main
+from typing import List, Callable, Optional, NoReturn
+import argparse
+import io_trace
+import random
+
+# TODO: two mains is confusing
+
+def main(get_test_cases: Callable[[JsonMetadata], List[Case]], rng_seed: int = 23) -> NoReturn:
+    parser = argparse.ArgumentParser(
+        description="""
+        Gradescope autograder for Python
+        """,
+    )
+    parser.add_argument("--summary", action="store_true", help="print a summary of tests after writing to results.json")
+    args = parser.parse_args()
+
+    # run the autograder
+    exit_code: int = 0
+
+    io_trace.init()
+    try:
+        random.seed(rng_seed)
+        exit_code = autograder_main(get_test_cases, args.summary)
+    finally:
+        io_trace.deinit()
+
+    exit(exit_code)
